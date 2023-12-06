906864ce7da045cea1791848b6e6ab062e8db68d
RenatoGalizzi <regalizzi@gmail.com>
2023-12-06 18:32:53 -0300

    bug import route

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import About from "./components/About/About"; // crear el componente
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart"; // crear el componente
 import Login from "./components/Login/Login"; // crear el componente
 import NavBar from "./components/NavBar/NavBar";
-import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   const location = useLocation();
