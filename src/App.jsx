c230c616c6a8ade726ecafef671b08a1ca335129
Sunny1606 <solescobares@gmail.com>
2023-12-05 22:29:01 -0300

    avance home/cards/carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import Button from 'react-bootstrap/Button';
-import CardComponent from './components/Card/Card';
+// import Button from 'react-bootstrap/Button';
 // import PATHROUTES from './helpers/pathroutes';
 import Home from "./components/Home/Home";
+import Carousel from "./components/Carousel/Carousel";
 
 function App() {
   return (
     <div>
-      <div>
-        <Button variant="outline-primary">Primary</Button>{' '}
-        <Button variant="outline-secondary">Secondary</Button>{' '}
-        <Button variant="outline-success">Success</Button>{' '}
-        <Button variant="outline-warning">Warning</Button>{' '}
-        <Button variant="outline-danger">Danger</Button>{' '}
-        <Button variant="outline-info">Info</Button>{' '}
-        <Button variant="outline-light">Light</Button>{' '}
-        <Button variant="outline-dark">Dark</Button>
-      </div>
-        <CardComponent />
+      <Carousel />
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
-
+// <div>
+// // // <Button variant="outline-primary">Primary</Button>{' '}
+// // // <Button variant="outline-secondary">Secondary</Button>{' '}
+// // // <Button variant="outline-success">Success</Button>{' '}
+// // // <Button variant="outline-warning">Warning</Button>{' '}
+// // // <Button variant="outline-danger">Danger</Button>{' '}
+// // // <Button variant="outline-info">Info</Button>{' '}
+// // // <Button variant="outline-light">Light</Button>{' '}
+// // // <Button variant="outline-dark">Dark</Button>
+// // <Home/>
+// //   </div>
