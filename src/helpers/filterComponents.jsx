d112827874645ddcd1990913bde7c115c2c52a14
RenatoGalizzi <regalizzi@gmail.com>
2023-12-06 21:38:26 -0300

    elimine default a indexComponent y filter

--- src/helpers/filterComponents.jsx
+++ src/helpers/filterComponents.jsx
@@ -3,4 +3,4 @@ import SizeBox from '../components/FilterBar/FilterBoxes/SizeBox/SizeBox';
 import ColorBox from '../components/FilterBar/FilterBoxes/ColorBox/ColorBox';
 import GenreBox from '../components/FilterBar/FilterBoxes/GenreBox/GenreBox';
 
-export default {PriceBox, SizeBox, ColorBox, GenreBox}
+export  {PriceBox, SizeBox, ColorBox, GenreBox}
