5527e19afdd09984c901b7ce73a80fe4fec9b85c
sport <sportvibe07@gmail.com>
2023-12-09 23:45:15 -0300

    avance

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,11 @@
 import styles from "./Home.module.css";
-import Carousel from "../../components/Carousel/Carousel";
 import ProductCard from "../../components/ProductCard/ProductCard";
-import FalseCard from "../../components/FalseCard/FalseCard";
 import FilterBar from "../FilterBar/FilterBar";
 import CategoryBar from "../FilterBar/CategoryBar/CategoryBar";
 import { CarouselComponent } from "../../helpers/indexComponents";
 import { useSelector } from "react-redux";
 
 function Home() {
-  const falseCards = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 4, 5, 4, 5, 4, 5, 4, 5, 4];
   const productRender = useSelector((state) => state.products);
 
   return (
@@ -21,10 +18,8 @@ function Home() {
           <FilterBar />
         </div>
         <div className={styles.conteinerHome}>
-          {/* <h2 className={styles.title}>Productos Recomendados</h2> */}
           <div className={styles.carouselHomeContainer}>
             <CarouselComponent text={['Productos Recomendados']} />
-            {/* <Carousel /> */}
           </div>
           <div className={styles.resultsContainer}>
             <p>Resultados: {productRender?.totalCount}</p>
