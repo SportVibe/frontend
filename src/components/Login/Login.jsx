294e3b4777838ba9b7aa15de358d9548bf0b8751
KYohanaGomez <kygg.16@hotmail.com>
2023-12-15 16:11:26 -0500

    cambios

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,125 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import LogoutButton from "../Logout/Logout";
-import Profile from "../Profile/Profile";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+//import google from "../../imagenes/google.png";
+import axios from "axios";
+//import { URL } from "../../config.js";
+import { GoogleAuthProvider, signInWithPopup, auth } from "../../firebase.js";
+import { LoginUser } from "../../redux/actions/actions.js";
+import { useDispatch } from "react-redux";
+
+const Login = () => {
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(false);
+  const [userRegistered, setUserRegistered] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const handleGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider(userData);
+
+    try {
+      setLoading(true);
+
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      const response = await axios.post(`${URL}Google`, {
+        firstName: user.displayName,
+        email: user.email,
+        roll: "Client",
+      });
+      console.log("response", response);
+
+      const receivedToken = response.data.token;
+      const id = response.data.id;
+      localStorage.setItem("token", receivedToken);
+
+      // Actualizar el estado si el usuario ya está registrado
+      setUserRegistered(true);
+      dispatch(
+        LoginUser({
+          firstName: user.displayName,
+          email: user.email,
+          roll: "Client",
+        })
+      );
+
+      console.log("LoginUser", LoginUser);
+
+      const userDataFromResponse = {
+        firstName: user.displayName,
+        email: user.email,
+        roll: "Client",
+        userId: id, // Use the userId from the response
+      };
+
+      if (id) {
+        createCart(userDataFromResponse.userId);
+      }
+
+      setUserData(userDataFromResponse);
+      window.location.href = "/Profile";
+    } catch (error) {
+      console.error("Error al autenticar con Google:", error.message);
+      console.error("Detalles del error:", error.response);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const createCart = async (userId) => {
+    try {
+      const { data } = await axios.post(`${URL}shoppingCart`, {
+        userId,
+      });
+      // console.log(data);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div className="container w-100 my-4">
+      <div className="row text-center">
+        <div className="col-12">Iniciar Sesión</div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button
+            type="button"
+            className="btn btn-outline-danger w-100 my-1"
+            onClick={handleGoogleSignIn}
+            disabled={userRegistered}
+          >
+            <div className="row align-items-center">
+              <div className="col-2">
+                <img src={google} alt="Google" width="32" className="" />
+              </div>
+              <div className="col-10 text-center">Google</div>
+            </div>
+          </button>
+        </div>
+      </div>
+      {loading && (
+        <div className="row text-center mt-2">
+          <div className="col-12">Cargando...</div>
+        </div>
+      )}
+      {userRegistered && userData && (
+        <div className="row text-center mt-2">
+          <div className="col-12"></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
+
+
+// import React from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import LogoutButton from "../Logout/Logout";
+// import Profile from "../Profile/Profile";
 
 // const LoginButton = () => {
 //   const { loginWithRedirect } = useAuth0();
@@ -11,26 +129,26 @@ import Profile from "../Profile/Profile";
 
 // export default LoginButton;
 
-function Login() {
-  const {isAuthenticated} =useAuth0();
+// function Login() {
+//   const {isAuthenticated} =useAuth0();
 
-  const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
-    return <button onClick={()=>loginWithRedirect()}>Login</button>
+//   const LoginButton = () => {
+//     const { loginWithRedirect } = useAuth0();
+//     return <button onClick={()=>loginWithRedirect()}>Login</button>
   
-  }
+//   }
 
-    return (
+//     return (
 
-      <div>
-        {isAuthenticated ? (<> <Profile/>
-       <LogoutButton/></>):<LoginButton/>}
+//       <div>
+//         {isAuthenticated ? (<> <Profile/>
+//        <LogoutButton/></>):<LoginButton/>}
        
-      </div>
+//       </div>
       
-    );
-  }
-  export default Login;
+//     );
+//   }
+//   export default Login;
 
 //   const validacion = ({usuario, password}) =>{
 //     let error = {};
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 import store from './redux/store';
 import App from './App';
 
@@ -10,20 +10,20 @@ import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider domain='sportvibe.us.auth0.com' 
-    clientId='VJvDwdnCWBZmJgqm6LjxvNc6HWCFIOIj' 
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-     // audience:"this is a unique identifier",
-    }}
-    scope='openid email profile'
-    >
 
     <Provider store={store}>
       <BrowserRouter>
+    {/* <Auth0Provider domain='sportvibe.us.auth0.com' 
+    clientId='VJvDwdnCWBZmJgqm6LjxvNc6HWCFIOIj' 
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      // audience:"this is a unique identifier",
+    }}
+    scope='openid email profile'
+    > */}
         <App />
+    {/* </Auth0Provider> */}
       </BrowserRouter>
     </Provider>
-    </Auth0Provider>
   </React.StrictMode>
 );
