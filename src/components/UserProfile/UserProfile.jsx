bb47c0272a3c0e2292ec5ed72dbd96d92ef442c7
sport <sportvibe07@gmail.com>
2023-12-24 21:38:19 -0300

    avance userProfile

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -100,57 +100,60 @@ function UserProfile() {
 
     return (
         <div className={styles.mainView}>
-            <div className={styles.subMainView}>
-                <div className={styles.sideBarContainer}>
-                    <div className={styles.headerSection}>
-                        <div className={styles.userDataPreview}>
-                            <div className={styles.imgContainer}>
-                                <div>
-                                    {imgUser ?
-                                        <img src={imgUser} alt="" /> :
-                                        <p>{firstName}{lastName}</p>
-                                    }
+            {!userDataRender ?
+                <Loading /> :
+                <div className={styles.subMainView}>
+                    <div className={styles.sideBarContainer}>
+                        <div className={styles.headerSection}>
+                            <div className={styles.userDataPreview}>
+                                <div className={styles.imgContainer}>
+                                    <div>
+                                        {imgUser ?
+                                            <img src={imgUser} alt="" /> :
+                                            <p>{firstName}{lastName}</p>
+                                        }
+                                    </div>
+                                </div>
+                                <p className={styles.name}>{upperLowerCase(firstNameFull)} {upperLowerCase(lastNameFull)}</p>
+                                <p className={styles.userCode}>Código de usuario: {id}</p>
+                            </div>
+                            <div className={styles.buttonsContainer}>
+                                <div className={mainComponent === 'editUser' ? styles.selectedProfile : styles.editProfile} id='editUser' onClick={handlerComponent}>
+                                    <i className="fa-regular fa-pen-to-square" id='editUser' onClick={handlerComponent}></i>
+                                    <p id='editUser' onClick={handlerComponent}>Editar</p>
+                                </div>
+                                <div onClick={handleSignOut} className={styles.editProfile}>
+                                    <p onClick={handleSignOut}>Cerrar sesión</p>
                                 </div>
                             </div>
-                            <p className={styles.name}>{upperLowerCase(firstNameFull)} {upperLowerCase(lastNameFull)}</p>
-                            <p className={styles.userCode}>Código de usuario: {id}</p>
                         </div>
-                        <div className={styles.buttonsContainer}>
-                            <div className={mainComponent === 'editUser' ? styles.selectedProfile : styles.editProfile} id='editUser' onClick={handlerComponent}>
-                                <i className="fa-regular fa-pen-to-square" id='editUser' onClick={handlerComponent}></i>
-                                <p id='editUser' onClick={handlerComponent}>Editar</p>
+                        <div className={styles.footerSideSection}>
+                            <div className={mainComponent === 'purchasesTable' ? styles.divSelected : styles.div} id='purchasesTable' onClick={handlerComponent}>
+                                <i className="fa-solid fa-cart-shopping" id='purchasesTable' onClick={handlerComponent}></i>
+                                <p id='purchasesTable' onClick={handlerComponent}>Historial de compra</p>
                             </div>
-                            <div onClick={handleSignOut} className={styles.editProfile}>
-                                <p onClick={handleSignOut}>Cerrar sesión</p>
+                            <div className={mainComponent === 'favorites' ? styles.divSelected : styles.div} id='favorites' onClick={handlerComponent}>
+                                <i className="fa-regular fa-heart" id='favorites' onClick={handlerComponent}></i>
+                                <p id='favorites' onClick={handlerComponent}>Mis favoritos</p>
+                            </div>
+                            <div className={mainComponent === 'searchHistory' ? styles.divSelected : styles.div}>
+                                <i className="fa-solid fa-magnifying-glass" id='searchHistory' onClick={handlerComponent}></i>
+                                <p id='searchHistory' onClick={handlerComponent}>Historial de búsqueda</p>
                             </div>
                         </div>
                     </div>
-                    <div className={styles.footerSideSection}>
-                        <div className={mainComponent === 'purchasesTable' ? styles.divSelected : styles.div} id='purchasesTable' onClick={handlerComponent}>
-                            <i className="fa-solid fa-cart-shopping" id='purchasesTable' onClick={handlerComponent}></i>
-                            <p id='purchasesTable' onClick={handlerComponent}>Historial de compra</p>
-                        </div>
-                        <div className={mainComponent === 'favorites' ? styles.divSelected : styles.div} id='favorites' onClick={handlerComponent}>
-                            <i className="fa-regular fa-heart" id='favorites' onClick={handlerComponent}></i>
-                            <p id='favorites' onClick={handlerComponent}>Mis favoritos</p>
-                        </div>
-                        <div className={mainComponent === 'searchHistory' ? styles.divSelected : styles.div}>
-                            <i className="fa-solid fa-magnifying-glass" id='searchHistory' onClick={handlerComponent}></i>
-                            <p id='searchHistory' onClick={handlerComponent}>Historial de búsqueda</p>
-                        </div>
+                    <div className={styles.mainComponentsContainer}>
+                        {mainComponent === 'purchasesTable' &&
+                            <div className={styles.componentContainer}>
+                                <Table records={null} />
+                            </div>}
+                        {mainComponent === 'editUser' &&
+                            <div className={styles.componentContainer}>
+                                <EditUser editUserData={editUserData} setEditUserData={setEditUserData} isValidEmail={isValidEmail} handleSubmit={handleSubmit} />
+                            </div>}
                     </div>
                 </div>
-                <div className={styles.mainComponentsContainer}>
-                    {mainComponent === 'purchasesTable' &&
-                        <div className={styles.componentContainer}>
-                            <Table records={null} />
-                        </div>}
-                    {mainComponent === 'editUser' &&
-                        <div className={styles.componentContainer}>
-                            <EditUser editUserData={editUserData} setEditUserData={setEditUserData} isValidEmail={isValidEmail} handleSubmit={handleSubmit} />
-                        </div>}
-                </div>
-            </div>
+            }
         </div>
     );
 }
