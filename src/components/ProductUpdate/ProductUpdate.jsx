0265a3fc31a285292ccad61b84e8147349b21a35
RenatoGalizzi <regalizzi@gmail.com>
2023-12-27 01:02:06 -0300

    avances formulario edicion productos

--- src/components/ProductUpdate/ProductUpdate.jsx
+++ src/components/ProductUpdate/ProductUpdate.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 function ProductUpdate({ data, setSelectedRow }) {
   const [dataProductUpdate, setDataProductUpdate] = useState(null); //data que viene del detail del back
   const [product, setProduct] = useState(""); //guardo las modificaciones - estado intermedio
-  const [isEditing, setEditing] = useState(false); // por ahora no lo aplicos
+  const [isEditing, setEditing] = useState(false);
   const [newProduct, setNewProduct] = useState(""); // cuando se guardan los cambios , este es el state que envio al back (put)
   useEffect(() => {
     axios(`${API_URL}/detail/${data.id}`)
@@ -16,7 +16,6 @@ function ProductUpdate({ data, setSelectedRow }) {
   }, []);
 
   function handleChange(e) {
-    //setEditing(true);
     setProduct({...product, [e.target.name]: e.target.value });
   }
 
@@ -40,6 +39,7 @@ function ProductUpdate({ data, setSelectedRow }) {
     let img = [];
     img = dataProductUpdate.Images.filter((img) => img !== image);
     setDataProductUpdate({ ...dataProductUpdate, Images: img });
+    console.log(newProduct);
   };
 
   const handleWidget = (e) => {
@@ -60,11 +60,11 @@ function ProductUpdate({ data, setSelectedRow }) {
   };
   return (
     <div className="d-flex flex-wrap justify-content-center">
-      <div className="d-flex">
+      <div className="d-flex px-5">
         <form className="d-flex flex-column">
-          <div className="form-group">
+          <div className="form-group bg-light rounded-pill mt-2">
             <label
-              className="d-flex justify-content-center mt-3"
+              className="d-flex justify-content-center p-2"
               for="formGroupExampleInput"
             >
               <h3 className="">ID : {dataProductUpdate?.id}</h3>
@@ -93,35 +93,13 @@ function ProductUpdate({ data, setSelectedRow }) {
               onChange={handleChange}
             >
               <option disabled selected>
-                {dataProductUpdate?.category} || Seleccione Nuevo Valor
+                {dataProductUpdate?.category} {"--->"} Seleccione Nueva Categoria
               </option>
               <option value="CALZADO">CALZADO</option>
               <option value="DEPORTE">DEPORTE</option>
               <option value="ROPA">ROPA</option>
             </select>
           </div>
-          {/* <div className="form-group">
-            <label for="formGroupExampleInput">CATEGORIA</label>
-            <br></br>
-            <input
-              type="text"
-              name="category"
-              onChange={handleChange}
-              className="form-control"
-              id="formGroupExampleInput"
-              value={product.category || dataProductUpdate?.category}
-            />
-          </div> */}
-          {/* <div className="form-group">
-            <label for="formGroupExampleInput">SUB-CATEGORIA</label>
-            <input
-              type="text"
-              name="subCategory"
-              onChange={handleChange}
-              className="form-control"
-              value={product.subCategory || dataProductUpdate?.subCategory}
-            />
-          </div> */}
           <div className="form-group pb-3">
             <label for="formGroupExampleInput"><h5>SUBCATEGORIA</h5></label>
             <select
@@ -131,7 +109,7 @@ function ProductUpdate({ data, setSelectedRow }) {
               onChange={handleChange}
             >
               <option disabled selected>
-                {dataProductUpdate?.subCategory} || Seleccione Nuevo Valor
+                {dataProductUpdate?.subCategory} {"--->"} Seleccione Nueva Sub-categoria
               </option>
               <option value="ZAPATILLAS">ZAPATILLAS</option>
               <option value="BOTINES">BOTINES</option>
@@ -197,6 +175,10 @@ function ProductUpdate({ data, setSelectedRow }) {
               value={product.Colors || dataProductUpdate?.Colors.toString()}
             />
           </div>
+          <div class="form-check form-switch my-3">
+            <input className="form-check-input mx-2" type="checkbox" role="switch" onClick={(e)=>console.log(e)} id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" for="flexSwitchCheckDefault"><h5>Modificar cantidad y talles</h5></label>
+          </div>
           <div className="form-group pb-3">
             <label for="formGroupExampleInput2"><h5>DESCRIPCION</h5></label>
             <textarea
@@ -259,9 +241,9 @@ function ProductUpdate({ data, setSelectedRow }) {
           </div>
         </form>
       </div>
-      <div className={isEditing ? "d-flex flex-wrap mt-5 bg-light rounded justify-content-center border pt-4" : "sr-only"}>
+      <div className={isEditing ? "d-flex flex-wrap mt-5 bg-light rounded justify-content-center border pt-5 my-5" : "sr-only"}>
         <div className="d-flex flex-column align-items-center">
-          <h2 className="d-flex justify-content-center">Actualización</h2>
+          <h2 className="d-flex justify-content-center pb-3"> <i className="bi bi-pencil-square px-2"></i>Actualización</h2>
           <label for="formGroupExampleInput"><h5>TITULO :</h5></label>
           {newProduct.title ? `${newProduct.title}` : ""}
           <br></br>
@@ -299,7 +281,7 @@ function ProductUpdate({ data, setSelectedRow }) {
           <br></br>
           <br></br>
           <label for="formGroupExampleInput"><h5>IMAGENES :</h5></label>
-          <div className="d-flex flex-wrap justify-content-center">
+          <div className="d-flex flex-wrap justify-content-center my-4">
             {newProduct.Images &&
               newProduct?.Images.map((image) => {
                 return (
