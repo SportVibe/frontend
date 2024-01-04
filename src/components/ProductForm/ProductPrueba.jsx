bb517bc6df8c0d017bed8d96a8675c25cf3c49e0
RenatoGalizzi <regalizzi@gmail.com>
2024-01-04 20:13:53 -0300

    cambio responsive sidebar dashboard

--- src/components/ProductForm/ProductPrueba.jsx
+++ src/components/ProductForm/ProductPrueba.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import DataTable from "react-data-table-component";
 import axios from "axios";
 import { API_URL } from "../../helpers/config";
+import { loadLanguages } from "i18next";
 
-const ProductPrueba = ({setSelectedRow}) => {
+const ProductPrueba = ({setSelectedRow, setVisibleSidebar, visibleSidebar}) => {
   const [productRender, setProductRender] = useState([]);
   const [productSearch, setProductSearch] = useState([]);
  
@@ -173,13 +174,18 @@ const ProductPrueba = ({setSelectedRow}) => {
     },
   };
 
+  const handleVisibleSidebar = () => {
+    setVisibleSidebar(!visibleSidebar);
+  }
+
   return (
     <>
-      <nav className="navbar navbar-ligth bg-body-secondary justify-content-center w-100 ">
-        {/* <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-tools ms-10" viewBox="0 0 16 16">
-            <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708M3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z" />
-          </svg> */}
-            <div className="d-flex">
+      <nav className="navbar navbar-ligth bg-body-secondary justify-content-between w-100 ">
+            <div className="">
+            <button type="button" class="btn btn-ligth btn-s" onClick={handleVisibleSidebar}><i className="bi bi-list fs-3"></i></button>
+            </div>
+            <div className="d-flex  mx-auto">
+            <i className="bi bi-search fs-3 me-2"></i>
               <input
                 className="form-control"
                 type="search"
