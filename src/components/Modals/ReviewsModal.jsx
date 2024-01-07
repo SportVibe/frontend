c218b065551277c16286910ab5e17ae2806ad7a8
sport <sportvibe07@gmail.com>
2024-01-07 01:59:03 -0300

    avance del review en el userProfile

--- src/components/Modals/ReviewsModal.jsx
+++ src/components/Modals/ReviewsModal.jsx
@@ -9,21 +9,19 @@ import { useNavigate } from 'react-router-dom';
 import Reviews from '../Reviews/Reviews';
 
 
-function LoginModal({setModal, modal, userId, productId}) {
-   
+function LoginModal({ setModal, modal, userId, productId }) {
 
     function handleCloseModal(e) {
         const id = e.target.id;
         if (id === 'outSideModal') {
             setModal(null)
         }
-
     }
 
     return (
         <div id='outSideModal' className={styles.mainView} onClick={handleCloseModal}>
             <div className={styles.reviewsContainer}>
-            <Reviews userId={userId} productId={productId} setModal={setModal}/>
+                <Reviews userId={userId} productId={productId} setModal={setModal} />
             </div>
         </div>
     )
