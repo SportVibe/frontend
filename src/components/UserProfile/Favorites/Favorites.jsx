268af91078444f94bb0111fe45bb350a6b858c77
sport <sportvibe07@gmail.com>
2024-01-15 06:30:09 -0300

    avance favoritos

--- src/components/UserProfile/Favorites/Favorites.jsx
+++ src/components/UserProfile/Favorites/Favorites.jsx
@@ -3,21 +3,26 @@ import ProductCard from '../../ProductCard/ProductCard';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { API_URL } from '../../../helpers/config';
+import Loading from '../../loading/Loading';
 
-function Favorites({ userDataRender }) {
+function Favorites({ userDataRender, reloadPage, setReloadPage }) {
     const [productRender, setProductRender] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     async function getFavoriteProducts() {
         try {
+            setLoading(true);
             const products = userDataRender?.favorites?.map(async (id) => {
                 const { data } = await axios(`${API_URL}/detail/${id}`);
                 return data.data;
             });
             const favoriteProducts = await Promise.all(products);
             // console.log(favoriteProducts);
-            setProductRender(favoriteProducts)
+            setProductRender(favoriteProducts);
+            setLoading(false);
         } catch (error) {
             console.error('Error al obtener los productos favoritos:', error);
+            setLoading(false);
         }
     }
 
@@ -27,17 +32,21 @@ function Favorites({ userDataRender }) {
 
     return (
         <div className={styles.mainVew}>
-            <div className={styles.suMainVew}>
-                <div className={styles.conteinerCards}>
-                    {productRender?.length > 0 && productRender.map((product, i) => {
-                        return (
-                            <div key={i} className={styles.cardComponentContainer}>
-                                <ProductCard productData={product} />
-                            </div>
-                        )
-                    })}
+            {loading ?
+                <Loading />
+                :
+                <div className={styles.suMainVew}>
+                    <div className={styles.conteinerCards}>
+                        {productRender?.length > 0 && productRender.map((product, i) => {
+                            return (
+                                <div key={i} className={styles.cardComponentContainer}>
+                                    <ProductCard productData={product} setFavReload={setReloadPage} favReload={reloadPage} />
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     );
 }
