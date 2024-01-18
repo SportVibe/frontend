a7ccf65f895671fa399a648a3178bec21e24f0a7
mangelicapj <mangelica.0894@gmail.com>
2024-01-18 15:43:12 -0500

    filtros sin numeritos XD

--- src/components/FilterBar/FilterBoxes/GenreBox/GenreBox.jsx
+++ src/components/FilterBar/FilterBoxes/GenreBox/GenreBox.jsx
@@ -19,7 +19,7 @@ function GenreBox({ genderStatistics, genreHandler, genre, sport, brand }) {
                 <p id='MUJER' onClick={genreHandler} className={genre[0].gender === 'MUJER' ? styles.selected : ''}>Mujer</p> */}
                 <p id='' onClick={genreHandler} className={genre[0].gender === '' ? styles.selected : ''}>Todo</p>
                 {genderStatistics && genderStatistics.length && genderStatistics.map((item, i) => {
-                    return <p key={i} id={item.gender} onClick={genreHandler} className={normalizeString(genre[0]?.gender.toUpperCase()) === normalizeString(item?.gender.toUpperCase()) ? styles.selected : ''}>{capitalize(item.gender)} <span>{`(${item.productCount})`}</span></p>
+                    return <p key={i} id={item.gender} onClick={genreHandler} className={normalizeString(genre[0]?.gender.toUpperCase()) === normalizeString(item?.gender.toUpperCase()) ? styles.selected : ''}>{capitalize(item.gender)}</p>
                 })}
             </div>
         </div>
